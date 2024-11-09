966cff3fa5701e5cb5fd9d3bfae2d7007cf91b45
Tomrm96 <100447956+Tomrm96@users.noreply.github.com>
2024-11-09 21:22:25 +0000

    My Answer
    Exercise 9 Your six initial numbers are: 12,11,7,5,3,2 and your target number is: 79

---
+++ Q1 12, 11, 7, 5, 3, 2 Target = 79 ARMLite .asm
@@ -0,0 +1,10 @@
+      MOV R0, #7
+      MOV R1, #3
+      ADD R2, R1 , R0
+      MOV R3, #5
+      SUB R4, R3, #2
+      LSL R5, R2, R4
+      MOV R6, #12
+      SUB R7, R6, #11
+      SUB R12, R5, R7
+      HALT
